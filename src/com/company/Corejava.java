3503d1cb22ad6e9be9dd7856318cf128ae8967f6
nahinakter <ochanameye@hotmail.com>
2019-11-13 14:05:03 +0100

    first commit

---
+++ src/com/company/Corejava.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import sun.awt.geom.AreaOp;
+
+public class Corejava {
+    public static void main(String[] args) {
+       int num1, num2 , result; //int typer doita variable
+           num1 =20; //variable moddhe value
+           num2 =10;
+
+           result = num1 + num2;
+           System.out.println("Sum= "+result);
+
+           result = num1 - num2;
+           System.out.println("Sub = "+result);
+
+           result= num1 * num2;
+           System.out.println("Multiplication = "+result);
+
+           result= num1 / num2;
+           System.out.println("Div= "+result);
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name;
+
+
+
+
+        System.out.println ("Enter any name : ");
+        name = input. nextLine ();
+
+        System.out.println ("welcome ="+name);
+
+
+
+
+    }
+}
